6d2c0daaa21d33a15479641ca2d551cc1259bd20
David Felix <davidffelix@hotmail.com>
2022-06-06 21:40:53 -0300

    Update app name and icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal.shade700,
+        backgroundColor: Colors.indigo.shade800,
         appBar: AppBar(
           title: Text('Dadoos'),
-          backgroundColor: Colors.teal.shade900,
+          backgroundColor: Colors.indigo.shade900,
         ),
         body: Dadoos(),
       ),
@@ -15,9 +17,47 @@ void main() {
   );
 }
 
-class Dadoos extends StatelessWidget {
+class Dadoos extends StatefulWidget {
+  @override
+  State<Dadoos> createState() => _DadoosState();
+}
+
+class _DadoosState extends State<Dadoos> {
+
+  int numeroDadoEsquerda = 1;
+  int numeroDadoDireita = 6;
+
+  void alterarFacesDosDados(){
+    setState(() {
+      numeroDadoEsquerda = Random().nextInt(6) + 1;
+      numeroDadoDireita = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed:(){
+                alterarFacesDosDados();
+              },
+              child: Image.asset('imagens/dado$numeroDadoEsquerda.png'),
+            ),
+          ),
+
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+                alterarFacesDosDados();
+              },
+              child: Image.asset('imagens/dado$numeroDadoDireita.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
